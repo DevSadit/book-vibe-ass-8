97c37c5ed529b36af5696c9046d23b858d22b6ff
DevSadit <de.sadit@gmail.com>
2024-03-26 11:17:10 +0600

    fake book data added

---
+++ src/Books/Books.jsx
@@ -0,0 +1,12 @@
+const Books = () => {
+    return (
+        <div className="mt-24">
+            <div className="text-center text-4xl font-bold"><h1>Books</h1></div>
+            <div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Books;
